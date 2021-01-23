5aef0184e1b7afb3a23a7cd72bdfec9a915511ec
mkloouo <mkloouo@gmail.com>
2021-01-23 16:29:00 +0200

    Encapsulate close

--- index.ts
+++ index.ts
@@ -1,21 +1,21 @@
 import {createInterface} from 'readline';
 
-const readlineInterface = createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
 /**
  * Prompt stdout with query and return an answer
  * @param query query
  */
-export const ask = async (query: string) => {
+const ask = async (query: string) => {
+  const readlineInterface = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
   return new Promise((resolve, _reject) => {
-    readlineInterface.question(query, resolve);
+    readlineInterface.question(query, (answer) => {
+      resolve(answer);
+      readlineInterface.close();
+    });
   });
 };
 
-/**
- * Mandatory call when all ask interaction ends
- */
-export const closeAsk = () => readlineInterface.close();
+export default ask;
